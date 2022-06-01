5794fc44d01d156612f9302d25f1d6ecdc2b4ec9
jonwilm <jparedes84@gmail.com>
2022-06-01 17:41:25 -0400

    Modelos

--- applications/livececilia/models.py
+++ applications/livececilia/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class LiveCecilia(models.Model):
 
-    firstname = models.CharField('Nombre', max_length=200)
-    lastname = models.CharField('Apellido', max_length=200)
-    date_nac = models.DateField('Fecha de Nacimiento', blank=True)
-    genere = models.CharField('Género', max_length=2, blank=True)
-    code_zip = models.CharField('Código Postal', max_length=10, blank=True)
-    city = models.CharField('Ciudad', max_length=3, blank=True)
+    firstname = models.CharField('Nombre', max_length=200, null=False)
+    lastname = models.CharField('Apellido', max_length=200, null=False)
+    date_nac = models.DateField('Fecha de Nacimiento', blank=True, null=True)
+    genere = models.CharField('Género', max_length=2, blank=True, null=True)
+    code_zip = models.CharField('Código Postal', max_length=10, blank=True, null=True)
+    city = models.CharField('Ciudad', max_length=3, blank=True, null=True)
     email = models.CharField('Email', max_length=80, unique=True, null=False)
-    phone = models.CharField('Teléfono', max_length=15, blank=True)
+    phone = models.CharField('Teléfono', max_length=15, blank=True, null=True)
     created_at = models.DateTimeField('Fecha de suscripción', auto_now=False, auto_now_add=True)
 
     class Meta:
--- applications/sorteo/models.py
+++ applications/sorteo/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Sorteo(models.Model):
 
-    firstname = models.CharField('Nombre', max_length=200)
-    lastname = models.CharField('Apellido', max_length=200)
-    date_nac = models.DateField('Fecha de Nacimiento')
-    genere = models.CharField('Género', max_length=2)
-    code_zip = models.CharField('Código Postal', max_length=10, blank=True)
-    city = models.CharField('Ciudad', max_length=3)
-    email = models.CharField('Email', max_length=80, unique=True)
-    phone = models.CharField('Teléfono', max_length=15)
+    firstname = models.CharField('Nombre', max_length=200, null=False)
+    lastname = models.CharField('Apellido', max_length=200, null=False)
+    date_nac = models.DateField('Fecha de Nacimiento', null=False)
+    genere = models.CharField('Género', max_length=2, null=False)
+    code_zip = models.CharField('Código Postal', max_length=10, blank=True, null=True)
+    city = models.CharField('Ciudad', max_length=3, null=False)
+    email = models.CharField('Email', max_length=80, unique=True, null=False)
+    phone = models.CharField('Teléfono', max_length=15, null=False)
     created_at = models.DateTimeField('Fecha de suscripción', auto_now=False, auto_now_add=True)
 
     class Meta:
